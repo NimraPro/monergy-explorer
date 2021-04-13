cff0ea57a2fdaf0cdd270d1b3f9499eec3bb7d59
NimraPro <ahmetovix@gmail.com>
2021-04-13 12:25:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,20 @@
-var api = 'http://pool.mangocoin.online:11898';
-var donationAddress = "MEpLh1LswBqihtwVB7VuYAQP7E39SYSwVQwFVyAjpGd6fdALVvZk74YTq5jTo4DNnTdkw2wcWCzJ2EtVJ9k9DhioBWQ7GGq";
-var blockTargetInterval = 120; // enter the block interval in seconds
-var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100000000 slices = 1 MNG
-var totalSupply =  2000000000000000000; // enter the total supply in atomic units
-var symbol = 'mng'; // enter the coin's ticker
+var api = 'http://nocode.cloud:11897';
+var donationAddress = "XEGYy2XzW7ubbbU1X5nQR6D6CaFrNLw8vVFfZFWv4vt8fuVHw7Z7Zm5Y2X5bQYjxo5FiYhXfj39eq36HBZDePHbWZPGZ8Uy6qyu";
+var blockTargetInterval = 90; // enter the block interval in seconds
+var coinUnits = 1000;  // enter in the amount of atomic units in 1 coin, eg. 1000 wE = 1 XEGY
+var totalSupply =  100000000000000; // enter the total supply in atomic units
+var symbol = 'XEGY'; // enter the coin's ticker
 var refreshDelay = 30000;
 
 // pools stats by MainCoins
 var networkStat = {
  "mng": [
-	["pool.mangocoin.online", "http://pool.mangocoin.online:3107"],
-	["webmng.semipool.com", "https://webmng.semipool.com/api"],
-	["mng.miningpool.fun", "https://mng.miningpool.fun:8111"],
-	["fastpool.xyz", "https://fastpool.xyz:10088"],
-	["mn.pool-pay.com", "http://mng.pool-pay.com:8727"],
-	["mng.cryptonote.club", "https://mng.cryptonote.club:8199"]
+	["nocode.cloud:11897", "http://nocode.cloud:11897"],
  ]
 };
 
 var networkStat2 = {
-    "mng": [
+    "XEGY": [
 	[""]
  ]
 };
